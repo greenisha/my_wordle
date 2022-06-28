13692b518883ff717225be7eb34f2b6d7ce8da36
Greenisha <greenisha@gmail.com>
2022-06-28 22:26:01 +0300

    local storage

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Keyboard from './Keyboard'
 import WordContainer from './WordContainer'
 import wordsBank from '../words';
+import wordsGuessBank from '../wordsGuess';
 import Success from './Success';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,16 +11,39 @@ function Game() {
   const [status, setstatus] = useState([]);
   const [word, setword] = useState('');
   const [success, setsuccess] = useState(false);
-  const defaultWord = '     ';
-  useEffect(() => {
-    setword(wordsBank[Math.floor(Math.random() * 3482)]);
-  }, [])
+  const [position, setposition] = useState({ wordPosition: 0, letterPosition: 0 })
 
+  const defaultWord = '     ';
   const [words, setwords] = useState({
     0: [...defaultWord], 1: [...defaultWord], 2: [...defaultWord]
     , 3: [...defaultWord], 4: [...defaultWord], 5: [...defaultWord]
   });
-  const [position, setposition] = useState({ wordPosition: 0, letterPosition: 0 })
+  useEffect(() => {
+    const firstday = new Date('2022-05-28');
+    const currentDate = new Date();
+    const daysPassed = Math.floor(((currentDate.getTime() - firstday.getTime()) / (1000 * 60 * 60 * 24)));
+    console.log(wordsGuessBank[daysPassed]);
+    setword(wordsGuessBank[daysPassed]);
+    if (localStorage.getItem('day') === null || localStorage.getItem('day')!=daysPassed)
+    {
+      console.log('clear');
+      localStorage.clear();
+      localStorage.setItem('day', daysPassed);
+    }
+    else
+    {
+      if (localStorage.getItem('status')!==null)
+      setstatus(JSON.parse(localStorage.getItem('status')));
+      if (localStorage.getItem('words') !== null)
+      setwords(JSON.parse(localStorage.getItem('words')));
+      if (localStorage.getItem('position') !== null)
+        setposition(JSON.parse(localStorage.getItem('position')));
+
+      localStorage.setItem('day', daysPassed);
+    }
+  }, [])
+
+  
   const pressLetter = (letter) => {
 
     const { wordPosition, letterPosition } = position;
@@ -34,6 +58,9 @@ function Game() {
 
     setwords(newWords);
     setposition({ wordPosition, letterPosition: letterPosition + 1 });
+    localStorage.setItem('position', JSON.stringify({ wordPosition, letterPosition:letterPosition  + 1 }));
+    localStorage.setItem('words', JSON.stringify(newWords));
+
   };
   const pressBackspace = () => {
     const { wordPosition, letterPosition } = position;
@@ -42,6 +69,8 @@ function Game() {
     const newWords = { ...words, [wordPosition]: newWord };
     setwords(newWords);
     setposition({ wordPosition, letterPosition: letterPosition - 1 });
+    localStorage.setItem('position', JSON.stringify({ wordPosition, letterPosition:letterPosition-1 }));
+    localStorage.setItem('words', JSON.stringify(newWords));
   };
   const pressEnter = () => {
     const { wordPosition, letterPosition } = position;
@@ -78,6 +107,9 @@ function Game() {
     setstatus([...status, ...newstatus]);
     console.log([...status, ...newstatus]);
     setposition({ wordPosition: wordPosition + 1, letterPosition: 0 });
+    localStorage.setItem('position',JSON.stringify({wordPosition: wordPosition+1,letterPosition : 0}));
+    localStorage.setItem('status', JSON.stringify([...status, ...newstatus]));
+
     if (wordPosition > 5) {
       toast(`попытки закончились, увы. Слово было ${word}`);
       return;
--- src/components/Success.js
+++ src/components/Success.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Success({shown}) {
   return (
       <>
-      {(shown === true) ? <div className='absolute top-0 left-0 w-screen h-screen flex items-center justify-center bg-white text-8xl text-center'>Поздравляю, Вы угадали!<br/>Обновите страницу, чтобы сыграть еще раз</div>:null}
+      {(shown === true) ? <div className='absolute top-0 left-0 w-screen h-screen flex items-center justify-center bg-white text-3xl text-center'>Поздравляю, Вы угадали!<br/>Обновите страницу, чтобы сыграть еще раз</div>:null}
     </>
   )
 }
